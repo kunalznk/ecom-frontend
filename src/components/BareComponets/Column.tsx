4ff9e9f003b59822fc4b4fee6a6c583a95e6bdcf
kunal.zanke <kunal.zanke@globallogic.com>
2023-01-06 10:27:45 +0530

    added bare components

---
+++ src/components/BareComponets/Column.tsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material"
+
+export const ColumnContainer  = ()  => {
+return <Grid container>
+    </Grid>
+}
+
+export const ColumnItem = () => {
+    return <Grid item>
+        </Grid>
+    }
---
+++ src/components/BareComponets/Row.tsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material"
+
+export const RowContainer = () => {
+return <Grid container>
+    </Grid>
+}
+
+export const RowItem = () => {
+    return <Grid item>
+        </Grid>
+    }
