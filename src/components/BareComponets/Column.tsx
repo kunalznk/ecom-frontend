6662eaf92ae6dab0030c2cfc30fff6358d0624fc
kunal.zanke <kunal.zanke@globallogic.com>
2023-01-11 10:35:45 +0530

    added row and column

--- src/components/BareComponets/Column.tsx
+++ src/components/BareComponets/Column.tsx
@@ -1,13 +1,16 @@
-import { Grid } from "@mui/material"
+import { Grid, GridTypeMap } from "@mui/material"
+import  { ReactElement } from "react";   
+import { DefaultComponentProps, OverridableComponent } from "@mui/material/OverridableComponent"
 
-export const ColumnContainer  = (props: any)  => {
-return <Grid container flexDirection="column"  sx={{border: '2px solid red'}}
+
+export const ColumnContainer  = (props: DefaultComponentProps<GridTypeMap<{}, "div">>) : ReactElement<OverridableComponent<GridTypeMap<{}, "div">>>  => {
+return <Grid container flexDirection="column" alignContent="center" border="5px solid red"
 {...props}
 >
     </Grid>
 }
 
-export const ColumnItem = (props: any) => {
-    return <Grid flexDirection="column" item {...props}>
+export const ColumnItem = (props: DefaultComponentProps<GridTypeMap<{}, "div">>) : ReactElement<OverridableComponent<GridTypeMap<{}, "div">>>=> {
+    return <Grid item flexDirection="column" alignContent="center" border="5px solid blue" {...props}>
         </Grid>
     }
--- src/components/BareComponets/Row.tsx
+++ src/components/BareComponets/Row.tsx
@@ -1,11 +1,13 @@
-import { Grid } from "@mui/material"
+import { Grid, GridTypeMap } from "@mui/material"
+import  { ReactElement } from "react";   
+import { DefaultComponentProps, OverridableComponent } from "@mui/material/OverridableComponent"
 
-export const RowContainer = (props: any) => {
-return <Grid container {...props}>
+export const RowContainer = (props: DefaultComponentProps<GridTypeMap<{}, "div">>) : ReactElement<OverridableComponent<GridTypeMap<{}, "div">>> => {
+return <Grid container justifyContent="center" border="5px dashed black" {...props}>
     </Grid>
 }
 
-export const RowItem = (props: any) => {
-    return <Grid item {...props}>
+export const RowItem = (props: DefaultComponentProps<GridTypeMap<{}, "div">>)  : ReactElement<OverridableComponent<GridTypeMap<{}, "div">>> => {
+    return <Grid item justifyContent="center" border="5px dashed green" {...props}>
         </Grid>
     }
