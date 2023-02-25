9c7abc486ec020d15f13c9f3b1aa548ab4d80ce5
kunal.zanke <kunal.zanke@globallogic.com>
2023-02-25 11:49:47 +0530

    clean prod

--- src/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-const SUCCESS_MESSAGES : {[key : string ]: string }= {
-"login/fulfilled" : "Login Successfully ! ",
-"register/fulfilled" : "Register Successfully !"
-}
-export {
-    SUCCESS_MESSAGES
-}
--- src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    setBreakPoints :  (xs: number , sm: number , md:number, lg:number, xl:number ) => {
-        return {
-            xs, sm, md , lg, xl
-        }
-    }
-}
-
-export const textOverfllow = {maxHeight:"30px", textOverflow:"ellipsis",overflow: "hidden", display: "-webkit-box",WebkitLineClamp: "2",WebkitBoxOrient: "vertical"};
